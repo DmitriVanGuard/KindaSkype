f5c641ef17227c9fe1fb4cf09a5d1e97248719ad
DmitriVanGuard <dimavanguard@gmail.com>
2018-06-23 15:59:16 +0300

    Store subscribed to render func. Fancy log func

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import store from './store';
+// import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+	fancyLog();
+	ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+render();
+store.subscribe(render);
+// registerServiceWorker();
+
+/* **** */
+/* HELPER FUNCTIONS */
+/* **** */
+function fancyLog() {
+	console.log(
+		`%c Rendered with -> `,
+		'background: purple; color: #FFF',
+		store.getState()
+	);
+	// console.log(store.getState());
+}
