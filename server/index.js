7e930b582c5f23ef07b923d37d2e89ced653ae5a
DmitriVanGuard <dimavanguard@gmail.com>
2018-06-24 21:25:56 +0300

    Server storing clients

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const io = require('socket.io')(http);
 
 const config = require('./config');
 
+const clients = new Map();
+
 app.use('*', (req, res, next) => {
 	if (req.originalUrl.indexOf('socket.io/') === -1) {
 		proxy(`${config.HOST}:${config.PROXY_PORT}`, {
@@ -16,19 +18,35 @@ app.use('*', (req, res, next) => {
 });
 
 io.on('connection', socket => {
-	console.log('a user connected');
+	console.log('New user has connected to the server...');
+
 	socket.on('disconnect', () => {
-		console.log('user has disconnected');
+		console.log(
+			`${socket.username} has disconnected and his socket was deleted...`
+		);
+		// console.log(Array.from(clients.keys()));
+		clients.delete(socket.username);
+		// console.log(Array.from(clients.keys()));
 	});
 
 	socket.on('login', username => {
 		console.log(username);
 		// setTimeout(() => {
+		if (username && !clients.has(username)) {
+			clients.set(username, socket);
+			socket.username = username;
+			socket.emit(
+				'login',
+				createAnswer('OK', `everything ok, your username ${username}`)
+			);
+		}
 		socket.emit(
 			'login',
-			createAnswer('OK', `everything ok, your username ${username}`)
+			createAnswer('ERROR', `Please, choose another username`)
 		);
+
 		// }, 500);
+		// console.log(io.sockets);
 	});
 });
 
