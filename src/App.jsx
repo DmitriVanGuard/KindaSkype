951b277ecee59eacf2cf50450ce05b3893de40d5
DmitriVanGuard <dimavanguard@gmail.com>
2018-06-25 23:41:22 +0300

    Junk removed

--- src/App.jsx
+++ src/App.jsx
@@ -22,32 +22,6 @@ Client.onReceiveMessage = ({ status, data }) => {
 	}
 };
 
-/* 
-1) this.onContactSearch$ = new Subject();
-
-Client.onReceiveMessage$ = new Subject();
-
---------------
-2) this.subscription = this.onContactSearch$.debounceTime(700).subscribe(username => {
-	console.log(username);
-	store.dispatch(
-		username !== '' ? searchContact(username) : resetContactSearch()
-	);
-});
-
-Client.addSubscription(
-	Client.onContactSearch$.subscribe(data => store.dispatch(receiveMessage(data)))
-)
-
---------------
-3) this.onContactSearch$.next(match); // when event occurs
-
-Client.onReceiveMessage$.next(data);
-
---------------
-4) if (this.subscription) this.subscription.unsubscribe();
-*/
-
 const App = () => (
 	<Provider store={store}>
 		<div className="App">
