9c75e452cb7acd3b5fd67fb5752cb614f0470f86
DmitriVanGuard <dimavanguard@gmail.com>
2018-06-25 22:42:19 +0300

    More junk removed

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import store from './store';
-import fancyLog from './utils/log';
+// import fancyLog from './utils/log';
 
 import App from './App';
 
 import './index.css';
 
 const render = () => {
-	fancyLog();
+	// fancyLog();
 	ReactDOM.render(<App />, document.getElementById('root'));
 };
 
